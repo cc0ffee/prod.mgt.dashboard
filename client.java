f01f0356ce0e08aab242947898f2cd685d78af55
icedamericanos <stasikkacper1@gmail.com>
2022-11-26 18:20:32 -0600

    comments

--- client.java
+++ client.java
@@ -3,27 +3,30 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class client {
-    
     public static void main(String[]args) {
-        ArrayList<item> Products = new ArrayList<item>();
-        item Apple = new item("Fruits","Apple", "Red", 2.99, 44, true);
+        /*
+        The product management system is here! It creates a list called Products to keep track of all products in a system.
+        The main menu is command-line, where users can input their information and choices in the command-line.
+        Users can add, search, and categorize products in this system through its menus.
+        */
+        ArrayList<item> Products = new ArrayList<item>(); // New array of products in system
+        item Apple = new item("Fruits","Apple", "Red", 2.99, 44, true); // A pre-set item
         Products.add(Apple);
         Scanner sc = new Scanner(System.in);
-        //item temp = new item(null, null, null, 0, 0, false);
         System.out.println("Welcome to the Product Management System!");
         int direction = 1; 
         while (direction != 0) {
             System.out.println("Input 1 to add product\nInput 2 to search product\nInput 3 to categorize products\nInput 0 to exit the system");
             direction = sc.nextInt();
-            while (direction < 0 || direction > 4) {
+            while (direction < 0 || direction > 4) { // user inputs number out of the options
                 System.out.println("Error!\nInput 1 to add product\nInput 2 to search product\nInput 3 to categorize products\nInput 0 to exit the system");
                 direction = sc.nextInt();
             }
-            switch(direction) {
+            switch(direction) { // based on direction, choose which case to execute
                 case 1:
                 Products = add.addProduct(Products);
                 System.out.println(Products.get(0).getName());
-                break;
+                break; // breaks will return to the start of the while loop, which is the main menu
                 case 2: 
                 search.searchProduct(Products);
                 break;
--- functions/category.java
+++ functions/category.java
@@ -3,17 +3,22 @@ import java.util.Scanner;
 import java.util.ArrayList;
 public class category {
     public static void listCategory(ArrayList<item> Products) {
-        ArrayList<String> category = new ArrayList<String>();
+        /* 
+        From main menu, show all available categories in current list and let user choose one category.
+        With chosen category, it will show all products under this category to which the user can view product details of if choose to.
+        After listing products, users can either list another category or return to main menu
+        */
+        ArrayList<String> category = new ArrayList<String>(); // New array to store the different categories
         for (int i = 0; i < Products.size(); i++) {
             String selectedItem = Products.get(i).getCategory();
-            if (!category.contains(selectedItem.toLowerCase())) {
+            if (!category.contains(selectedItem.toLowerCase())) { // checks if category is already in list, so there are no repeats
                 category.add(selectedItem);
             }
         }
         Scanner input = new Scanner(System.in);
         boolean listing = true;
         while (listing) {
-            if (category.size() == 0) {
+            if (category.size() == 0) { // returns to main menu because there are no products
                 System.out.println("No categories to search through....");
                 listing = false;
                 break;
@@ -23,11 +28,11 @@ public class category {
                 System.out.println((i+1) + ". " + category.get(i));
             }
             int selected_number = input.nextInt();
-            while (selected_number < 1 || selected_number > category.size()) {
+            while (selected_number < 1 || selected_number > category.size()) { // user inputs number out of the options
                 System.out.println("Please input correct number to a category: ");
                 selected_number = input.nextInt();
             }
-            ArrayList<item> selectedCategory = new ArrayList<item>();
+            ArrayList<item> selectedCategory = new ArrayList<item>(); // new array to keep list of products in current category
             for (int i = 0; i < Products.size(); i++) {
                 if (Products.get(i).getCategory().toLowerCase().equals(category.get(selected_number-1).toLowerCase())) {
                     selectedCategory.add(Products.get(i));
@@ -41,19 +46,19 @@ public class category {
                 }
                 System.out.println("Choose a product for more details, or enter 0 exit category");
                 selected_number = input.nextInt();
-                while (selected_number < 0 || selected_number > category.size()) {
+                while (selected_number < 0 || selected_number > category.size()) { // user inputs number out of the options
                     System.out.println("Please input correct number:");
                     selected_number = input.nextInt();
                 }
                 if (selected_number == 0) {
-                    inCategory = false;
+                    inCategory = false; // returns to exit menu choice
                 } 
                 else {
-                    System.out.println(selectedCategory.get(selected_number-1).toString());
+                    System.out.println(selectedCategory.get(selected_number-1).toString()); // shows product details that was chosen
                     System.out.println("Select in category again? (Y/N): ");
                     char code = input.next().charAt(0);
                     if (code == 'n' || code == 'N') {
-                        inCategory = false; // makes it return to main menu
+                        inCategory = false; // makes it return to category choice
                     }
                 }
             }
